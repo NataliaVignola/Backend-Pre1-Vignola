df27477a8793dd43808d27cd2610b37eca7ec76f
Natalia Vignola <natalia.vignola@hotmail.com>
2023-07-26 12:03:54 -0300

    first commit pre1

---
+++ src/ProductManager.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+
+class ProductManager {
+    //Constructor que recibe la ruta del archivo de productos
+    constructor(path) {
+        this.path = path;
+        this.products = [];
+        this.loadProducts();
+    }
+
+    //Método para cargar los productos desde el archivo JSON
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf-8');
+            this.products = JSON.parse(data);
+        } catch (error) {
+            console.log('Error loading products:', error.message);
+            this.products = [];
+        }
+    }
+
+    //Método para guardar los productos en el archivo JSON
+    saveProducts() {
+        try {
+            const data = JSON.stringify(this.products, null, 2);
+            fs.writeFileSync(this.path, data, 'utf-8');
+        } catch (error) {
+            console.log('Error saving products:', error.message);
+        }
+    }
+
+    //Método para obtener el último ID existente en el array de products
+    getLastProductId() {
+        if (this.products.length === 0) {
+            return 0;
+        }
+        return this.products[this.products.length - 1].id;
+    }
+
+    //Método para agregar un nuevo producto al array de products
+    addProduct(product) {
+        const newProduct = {
+            ...product,
+            id: this.getLastProductId() + 1 //Generando un nuevo ID
+        };
+
+        this.products.push(newProduct);
+        this.saveProducts();
+    }
+
+    //Método para actualizar un producto existente por su ID
+    updateProduct(id, fieldsToUpdate) {
+        const productIndex = this.products.findIndex((product) => product.id === id);
+        if (productIndex !== -1) {
+            const product = this.products[productIndex];
+            const updatedProduct = {
+                ...product,
+                ...fieldsToUpdate
+            };
+            this.products[productIndex] = updatedProduct;
+            this.saveProducts();
+        } else {
+            console.log('Product not found');
+        }
+    }
+
+    //Método para eliminar un producto existente por su ID
+    deleteProduct(id) {
+        this.products = this.products.filter((product) => product.id !== id);
+        this.saveProducts();
+    }
+
+    //Método para obtener los productos
+    getProducts() {
+        return this.products;
+    }
+
+    //Método para obtener un producto por ID
+    getProductById(id) {
+        const product = this.products.find((product) => product.id === id);
+        return product;
+    }
+}
+
+module.exports = ProductManager;
---
+++ src/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const ProductManager = require('./ProductManager');
+
+const app = express();
+const port = 8080; //Puerto a 8080 según la consigna de la preentrega
+
+app.use(express.json());
+
+//Instancia del ProductManager
+const productManager = new ProductManager('products.json');
+
+//Endpoint para obtener los productos
+app.get('/api/products', (req, res) => {
+    const {
+        limit
+    } = req.query;
+    let products = productManager.getProducts();
+
+    if (limit) {
+        const limitNum = parseInt(limit);
+        products = products.slice(0, limitNum);
+    }
+
+    res.json(products);
+});
+
+//Endpoint para obtener los producto por su ID
+app.get('/api/products/:pid', (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const product = productManager.getProductById(productId);
+
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({
+            error: 'Product not found'
+        });
+    }
+});
+
+//Ruta POST para agregar un nuevo producto
+app.post('/api/products', (req, res) => {
+    const newProduct = req.body;
+    productManager.addProduct(newProduct);
+    res.json(newProduct);
+});
+
+//Ruta PUT para actualizar un producto por su ID
+app.put('/api/products/:pid', (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const fieldsToUpdate = req.body;
+    productManager.updateProduct(productId, fieldsToUpdate);
+    res.json({
+        message: 'Product updated successfully'
+    });
+});
+
+//Ruta DELETE para eliminar un producto por su ID
+app.delete('/api/products/:pid', (req, res) => {
+    const productId = parseInt(req.params.pid);
+    productManager.deleteProduct(productId);
+    res.json({
+        message: 'Product deleted successfully'
+    });
+});
+
+//Inicio servidor
+app.listen(port, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${port}`);
+});
